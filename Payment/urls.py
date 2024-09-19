2ab847681a68595078505e7bcabf10861d1a153b
AristideAbeng <abengaristide@gmail.com>
2024-09-19 10:15:02 +0100

    latest changes

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PaymentStatusView,InitiatePaymentView, NotchPayWebhookView, VerifyPaymentView, PaymentSSEView
+from .views import PaymentStatusView,InitiatePaymentView, NotchPayWebhookView, VerifyPaymentView
 
 urlpatterns = [
     # Other URL patterns...

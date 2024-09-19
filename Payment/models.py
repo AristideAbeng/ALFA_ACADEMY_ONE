97981d33a22885bb480dba25b55f5bc3adf34cf2
AristideAbeng <abengaristide@gmail.com>
2024-09-19 10:36:23 +0100

    latest changes

--- Payment/models.py
+++ Payment/models.py
@@ -8,10 +8,16 @@ class Payment(models.Model):
         ('failed', 'Failed'),
     ]
 
+    PAYMENT_CURRENCY_CHOICES = [
+        ('XAF', 'XAF'),
+
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES, default='pending')
+    currency = models.CharField(max_length=7, choices=PAYMENT_CURRENCY_CHOICES, default='XAF')
     transaction_id = models.CharField(max_length=100, null=True, blank=True)  # Optional, if payment gateway provides this
     payment_method = models.CharField(max_length=100, null=True, blank=True)
 
